37f484e7bb096e24304d24ed7ab870de4fb4f209
Ricky Dall'Armellina <rdall96@gmail.com>
2022-09-24 01:37:59 +0000

    Resolve "Environment variables for server.properties in startup script"

--- minecraft/start_server.sh
+++ minecraft/start_server.sh
@@ -1,5 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 # Entry point for starting the minecraft server
+
+set -o errexit
 
 # Print out the java version
 echo "Java Runtime:"
@@ -9,24 +11,35 @@ echo -e "\n"
 # Set EULA
 echo "eula=$EULA" > eula.txt
 
-# Collect environment variables
-# $GAMEMODE
-# $ENABLE_COMMAND_BLOCKS
-# $MOTD
-# $PVP
-# $GENERATE_STRUCTURES
-# $DIFFICULTY
-# $MAX_PLAYERS
-# $ALLOW_FLIGHT
-# $VIEW_DISTANCE
-# $ALLOW_NETHER
-# $SIMULATION_DISTANCE
-# $PLAYER_IDLE_TIMEOUT
-# $WHITE_LIST
-# $SPAWN_NPCS
-# $SPAWN_ANIMALS
-# $SPAWN_MONSTERS
-# $SPAWN_PROTECTION
+# Collect environment variables (key = env var, value = minecraft name)
+declare -A PROPERTIES
+PROPERTIES[GAMEMODE]='gamemode'
+PROPERTIES[ENABLE_COMMAND_BLOCKS]='enable-command-blocks'
+PROPERTIES[MOTD]='motd'
+PROPERTIES[PVP]='pvp'
+PROPERTIES[GENERATE_STRUCTURES]='generate-structures'
+PROPERTIES[DIFFICULTY]='difficulty'
+PROPERTIES[MAX_PLAYERS]='max-players'
+PROPERTIES[ALLOW_FLIGHT]='allow-flight'
+PROPERTIES[VIEW_DISTANCE]='view-distance'
+PROPERTIES[ALLOW_NETHER]='allow-nether'
+PROPERTIES[SIMULATION_DISTANCE]='simulation-distance'
+PROPERTIES[PLAYER_IDLE_TIMEOUT]='player-idle-timeout'
+PROPERTIES[HARDCORE]='hardcore'
+PROPERTIES[WHITE_LIST]='white-list'
+PROPERTIES[SPAWN_NPCS]='spawn-npcs'
+PROPERTIES[SPAWN_ANIMALS]='spawn-animals'
+PROPERTIES[SPAWN_MONSTERS]='spawn-monsters'
+PROPERTIES[SPAWN_PROTECTION]='spawn-protection'
+
+# Wipe the server.properties file and re-write it with any overrides found in environment variables
+echo '' > server.properties
+for key in "${!PROPERTIES[@]}"; do
+    # Check if environment variable is set
+    if [[ -n "${!key}" ]]; then
+        echo "${PROPERTIES[$key]}=${!key}" >> server.properties
+    fi
+done
 
 # Display current server.properties
 echo -e "Server properties:\n"
--- run.sh
+++ run.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
+set -o errexit
+
 WORK_DIR=$(dirname $0)
 
 IMAGE_NAME="rdall96/minecraft-server"
 
 # Check docker version
-docker --version || exit 1
+docker --version
 
 # Check python version
-python3 -V || exit 1
+python3 -V
 
 # Create Python environment
 echo -e "\nSetting up build environment..."
 PYTHON_VENV="$WORK_DIR/venv"
 if [[ ! -d "$PYTHON_VENV" ]]; then
-    python3 -m venv "$PYTHON_VENV" || exit 1
+    python3 -m venv "$PYTHON_VENV"
 fi
 source "$PYTHON_VENV/bin/activate"
 
@@ -24,23 +26,26 @@ pip install -r "$WORK_DIR/requirements.txt"
 
 # Check if the image has already been created
 echo -e "\nChecking the latest Minecraft version..."
-python "$WORK_DIR/src/check_version.py" || exit 1
+python "$WORK_DIR/src/check_version.py"
 LATEST_VER=$(cat "$WORK_DIR/minecraft/latest.txt")
-docker manifest inspect $IMAGE_NAME:$LATEST_VER > /dev/null
+set +o errexit
+docker manifest inspect "$IMAGE_NAME:$LATEST_VER" > /dev/null
+FOUND="$(echo $?)"
+set -o errexit
 
-if [[ "$(echo $?)" == 0 ]]; then
+if [[ $FOUND == 0 ]]; then
     # The image exists abort
     echo -e "\nFound image for latest Minecraft version in DockerHub '$IMAGE_NAME:$LATEST_VER', no need to rebuild"
     exit 0
 else
     # Run the minecraft download script
     echo -e "\nDownloading Minecraft..."
-    python "$WORK_DIR/src/main.py" || exit 1
+    python "$WORK_DIR/src/main.py"
     MINECRAFT_VER=$(cat "$WORK_DIR/minecraft/server/version.txt")
 
     # Build the docker image
     echo -e "\nBuilding image..."
-    docker build "$WORK_DIR/minecraft" -t "$IMAGE_NAME:$MINECRAFT_VER" || exit 1
+    docker build "$WORK_DIR/minecraft" -t "$IMAGE_NAME:$MINECRAFT_VER"
     echo -e "\nTagged Minecraft docker image '$IMAGE_NAME:$MINECRAFT_VER' (latest)"
     docker tag "$IMAGE_NAME:$MINECRAFT_VER" "$IMAGE_NAME:latest"
 

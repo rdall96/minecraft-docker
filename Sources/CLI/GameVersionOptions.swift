9d70b3c01a06bc520b14c3a79900e47392fd584a
Ricky Dall'Armellina <rickydallarmallina@gmail.com>
2024-10-14 12:06:20 -0400

    Updated CLI command interface to support specifying a custom mod loader version
    - Only fabric and forge can specofy a mod loader version
    - Help menu shows you how to specify a mod loader version

--- Sources/CLI/GameVersionOptions.swift
+++ Sources/CLI/GameVersionOptions.swift
@@ -13,12 +13,21 @@ struct GameVersionOptions: ParsableArguments {
     @Option(name: .shortAndLong, help: "The type of Minecraft server.")
     var type: GameType = .vanilla
     
-    @Option(name: .shortAndLong, help: "The game version to use (i.e.: 1.20.1). \"latest\" and \"all\" are also valid options.")
+    @Option(name: .shortAndLong, help: "The game version to use (i.e.: 1.20.1). \"latest\" and \"all\" are also valid options. For modded Minecraft, you can specify the mod loader version like so <minecraft_version>:<mod_loader_version_string>")
     var version: GameVersion = .latest
     
     func validate() throws {
         if version.minecraft.isEmpty {
             throw ValidationError("The Minecraft version cannot be empty")
+        }
+        // only some types support specifying a custom mod loader version
+        if version.modLoader != nil {
+            switch type {
+            case .fabric, .forge:
+                break
+            default:
+                throw ValidationError("Specifying a mod loader version is not supported for \(type.rawValue)")
+            }
         }
     }
     
